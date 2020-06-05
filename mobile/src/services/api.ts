5725dc85e735436eba92397ef5f3146e1e450e50
HeitorMythicDev <heitormoraes777outlook.com>
2020-06-05 19:31:50 -0300

    ajustando ip da maquina para ficar igual dos outros arquivos

--- mobile/src/services/api.ts
+++ mobile/src/services/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-baseURL:'http://192.168.15.13:3333'//colocar o IP da máquina que aparece no app do expo aqui
+baseURL:'http://192.168.15.8:3333'//colocar o IP da máquina que aparece no app do expo aqui
 
 });
 
